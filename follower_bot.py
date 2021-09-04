fc8fab39e88c856c223c3018f8108f21ce9aa0f9
Peter <peter279k@gmail.com>
2021-09-04 15:48:16 +0800

    Add followers.txt file to store login name

--- follower_bot.py
+++ follower_bot.py
@@ -16,6 +16,12 @@ update_followed_user = 'https://api.github.com/user/following/%s'
 page = 1
 follower_counter = 0
 
+file_handler = open('./followers.txt', 'r')
+follower_txt_lists = ''.join(file_handler.readlines()).split('\n')[0:-1]
+file_handler.close()
+
+file_handler = open('./followers.txt', 'a')
+
 while True:
     response = requests.get(follower_url + str(page))
     follower_lists = json.loads(response.text)
@@ -26,15 +32,20 @@ while True:
     for follower_info in follower_lists:
         headers = { 'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36' }
         user = follower_info['login']
+        if user in follower_txt_lists:
+            continue
         response = requests.put(update_followed_user % (user), auth=HTTPBasicAuth(github_user, personal_github_token), headers=headers)
         if response.status_code == 204:
             print('User: %s has been followed!' % user)
+            file_handler.write(user + '\n')
         else:
             print(response.text)
     page += 1
 
+file_handler.close()
+
 file_handler = open('follower_counter.txt', 'w')
 file_handler.write(str(follower_counter) + '\n')
-
+file_handler.close()
 
 print('\nFollowing users from your followed lists are done!')
